ba34063a6a896552318b897da5ccfe6dd8da3fa8
Mike Wright <m.wright92@icloud.com>
2022-07-01 11:59:36 -0700

    WIP hardcode kegs

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React from 'react';
 import Keg from './Keg';
+
+const mainKegList = [
+  {
+    name: "beer",
+    brand: "PBR",
+    price: "$3",
+    alcoholContent: "5%"
+  },
+  {
+    name: "beer",
+    brand: "Rainier",
+    price: "$3",
+    alcoholContent: "5%"
+  },
+  {
+    name="beer"
+    brand="Four Loko"
+    price="$5"
+    alcoholContent="5%"
+  }
+]
 
 function KegList() {
   return (
     <React.Fragment>
-      <Keg 
-        name="beer"
-        brand="PBR"
-        price="$3"
-        alcoholContent="5%"/>
-      <Keg 
-        name="beer"
-        brand="Rainier"
-        price="$3"
-        alcoholContent="5%"/>
+      <Keg />
     </React.Fragment>
     
-  )
+    )
 }
 
 export default KegList
